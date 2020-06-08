1e344d7368e01ca759a4797a7b6ed7583c8787ff
贾祥凯 <285316882@qq.com>
2020-06-08 22:21:40 +0800

    ✨feat: 初始化项目

---
+++ src/views/app/app.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { Button } from "antd";
+
+export default function App() {
+  return (
+    <Fragment>
+      {/* <Button type="dashed">测试测试</Button> */}
+
+      <h2>叮咚——待办事项</h2>
+    </Fragment>
+  );
+}
