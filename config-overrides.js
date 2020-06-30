250c8d0bda0eae770e7869e11f6868105da86b99
贾祥凯 <285316882@qq.com>
2020-06-30 14:54:09 +0800

    feat: 添加devServer

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ const {
   addWebpackAlias,
   fixBabelImports,
   addWebpackPlugin,
+  overrideDevServer,
 } = require("customize-cra");
 const path = require("path");
 // const fs = require("fs");
@@ -15,32 +16,55 @@ const packageJson = require(path.join(__dirname, "package.json"));
 //   if (err) throw err;
 //   packageJson = JSON.parse(data);
 // });
-module.exports = override(
-  addWebpackPlugin(
-    new DefinePlugin({
-      __SERVICE_NAME__: JSON.stringify(packageJson.serviceName || null),
-      __PROJECT_NAME__: JSON.stringify(packageJson.name),
-      __PROJECT_DISPLAY_NAME__: JSON.stringify(packageJson.displayName),
-      __PROJECT_COMMIT_SHA__: JSON.stringify(repoInfo.sha),
-      __PROJECT_VERSION__: JSON.stringify(repoInfo.lastTag),
-      __PROJECT_AUTHOR__: JSON.stringify(repoInfo.author),
-      __PROJECT_COMMITS_SINCE_RELEASE__: JSON.stringify(
-        repoInfo.commitsSinceLastTag
-      ),
-      __PROJECT_ENVIRONMENT__: JSON.stringify(
-        process.env.NODE_ENV === "production" ? "prod" : "dev"
-      ),
-      __PROJECT_COMPILE_TIME__: JSON.stringify(Date.now()),
+
+const devServerConfig = () => (config) => {
+  return {
+    ...config,
+    proxy: {
+      "/api": {
+        target: "https://jiaxiangkai.cn",
+        changeOrigin: true,
+        logLevel: "debug",
+        secure: false,
+        pathRewrite: { "^/api": "" },
+        onProxyRes(proxyRes) {
+          proxyRes.headers["Cache-Control"] =
+            "no-cache, no-store, must-revalidate";
+        },
+      },
+    },
+  };
+};
+
+module.exports = {
+  webpack: override(
+    addWebpackPlugin(
+      new DefinePlugin({
+        __SERVICE_NAME__: JSON.stringify(packageJson.serviceName || null),
+        __PROJECT_NAME__: JSON.stringify(packageJson.name),
+        __PROJECT_DISPLAY_NAME__: JSON.stringify(packageJson.displayName),
+        __PROJECT_COMMIT_SHA__: JSON.stringify(repoInfo.sha),
+        __PROJECT_VERSION__: JSON.stringify(repoInfo.lastTag),
+        __PROJECT_AUTHOR__: JSON.stringify(repoInfo.author),
+        __PROJECT_COMMITS_SINCE_RELEASE__: JSON.stringify(
+          repoInfo.commitsSinceLastTag
+        ),
+        __PROJECT_ENVIRONMENT__: JSON.stringify(
+          process.env.NODE_ENV === "production" ? "prod" : "dev"
+        ),
+        __PROJECT_COMPILE_TIME__: JSON.stringify(Date.now()),
+      })
+    ),
+    // 配置路径别名
+    addWebpackAlias({
+      "@/": path.resolve(__dirname, "src/"),
+    }),
+    // antd 按需加载
+    fixBabelImports("import", {
+      libraryName: "antd",
+      libraryDirectory: "es",
+      style: true,
     })
   ),
-  // 配置路径别名
-  addWebpackAlias({
-    "@": path.resolve(__dirname, "src/"),
-  }),
-  // antd 按需加载
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true,
-  })
-);
+  devServer: overrideDevServer(devServerConfig()),
+};
